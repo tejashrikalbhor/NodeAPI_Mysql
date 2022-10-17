09a1d77141f49bc4ac083eaaa79cfe62c7fc2bd8
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-10-17 12:24:44 +0530

    index

--- nodeApi/src/index.js
+++ nodeApi/src/index.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const api = require('./api/node_api');
+const app = express();
+app.use(bodyParser.json());
+app.use('/demo',app);
 module.exports={}
