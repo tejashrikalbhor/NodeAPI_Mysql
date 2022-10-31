6641c5713f640ba4a4fafab7ce8e3aec0fb8f6ea
tejashrikalbhor <tejashri.kalbhor@siliconstack.com.au>
2022-10-31 11:57:54 +0530

    called get api to check the routes are working

--- nodeApi/src/api/bankDbApi.js
+++ nodeApi/src/api/bankDbApi.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const bankDb = express.Router();
 var mysql = require('mysql2');
 var mysql_ =require('mysql');
-const { Router } = require('express');
+// const { Router } = require('express');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -18,3 +18,20 @@ con.connect(function (err) {
         console.log("Connected!");
     }
 });
+bankDb.get('/db',async(req,res)=>{
+    try{
+        return res.status(200).json({Message:"ok"});
+        // const sql = `call GET_STUDENT_DETAILS()`;
+        // con.query(sql,(err,results,fields)=>{
+        //     if(err)throw err;
+        //     else{
+        //         console.log("result:",results[0].length)
+        //         return res.status(200).json({Result:results[0]});
+        //     }
+        // });  
+    }
+    catch(err){
+        return res.status(500).json({Error:err.Message()});
+    }
+});
+module.exports ={bankDb}
--- nodeApi/src/index.js
+++ nodeApi/src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {routes} = require('./api/node_api');
-const bankDb = require('./api/bankDbApi');
+const {bankDb} = require('./api/bankDbApi');
 const app = express();
 app.use(bodyParser.json());
 app.use('/demo',routes);
-app.use('/bankDb',routes);
+app.use('/bankDb',bankDb);
 
 module.exports=app ;
